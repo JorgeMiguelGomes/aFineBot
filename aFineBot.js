4e1a39af2704528e957636926687e9efa2059ed7
Jorge Gomes <jmctgomes@protonmail.com>
2019-06-27 16:21:09 +0100

    [Feat] Cleaned the code

--- aFineBot.js
+++ aFineBot.js
@@ -27,102 +27,104 @@ var sleep = require('system-sleep');
 // Start once
 tweeter();
 
-// Once every N milliseconds
-setInterval(tweeter, 60*60*1000);
+  // Once every N milliseconds
+  setInterval(tweeter, 60*60*1000);
 
-// Here is the bot!
-function tweeter() { 
+  // Here is the bot!
+  function tweeter() { 
 
-    // Run the Processing sketches
-  // You need to have Processing command line installed
-  // See: https://github.com/processing/processing/wiki/Command-Line
+      // Run the Processing sketches
+      // You need to have Processing command line installed
+      // See: https://github.com/processing/processing/wiki/Command-Line
 
-  var cmd = 'processing-java --sketch=`pwd`/Shape01/ --run';
-  exec(cmd, processing);
-  console.log('Generating Shape.png FILE');
+      var cmd = 'processing-java --sketch=`pwd`/Shape01/ --run';
+      exec(cmd, processing);
+      console.log('Generating Shape.png FILE');
   
-  sleep(10*1000); // sleep for 10 seconds
-  console.log('Sleeping 10 seconds');
+      sleep(10*1000); // sleep for 10 seconds
+      console.log('Sleeping 10 seconds');
 
-  var cmd = 'processing-java --sketch=`pwd`/Shape02/ --run';
-  exec(cmd, processing);
-  console.log('Generating Shape01.png file');
+      var cmd = 'processing-java --sketch=`pwd`/Shape02/ --run';
+      exec(cmd, processing);
+      console.log('Generating Shape01.png file');
 
-  sleep(10*1000); // sleep for 10 seconds
-  console.log('Sleeping 10 seconds');
+      sleep(10*1000); // sleep for 10 seconds
+      console.log('Sleeping 10 seconds');
 
-  var cmd = 'processing-java --sketch=`pwd`/Shape03/ --run';
-  exec(cmd, processing);
-  console.log('Generating Shape02.png file');
+      var cmd = 'processing-java --sketch=`pwd`/Shape03/ --run';
+      exec(cmd, processing);
+      console.log('Generating Shape02.png file');
 
-  sleep(10*1000); // sleep for 10 seconds
-  console.log('Sleeping 10 seconds');
+      sleep(10*1000); // sleep for 10 seconds
+      console.log('Sleeping 10 seconds');
 
-  var cmd = 'processing-java --sketch=`pwd`/Shape04/ --run';
-  exec(cmd, processing);
-  console.log('Generating Shape03.png file');
+      var cmd = 'processing-java --sketch=`pwd`/Shape04/ --run';
+      exec(cmd, processing);
+      console.log('Generating Shape03.png file');
 
-  sleep(10*1000); // sleep for 10 seconds
-  console.log('Sleeping 10 seconds');
+      sleep(10*1000); // sleep for 10 seconds
+      console.log('Sleeping 10 seconds');
 
-  var cmd = 'processing-java --sketch=`pwd`/RandomLetters/ --run';
-  exec(cmd, processing);
-  console.log('Generating final.png file');
+      var cmd = 'processing-java --sketch=`pwd`/RandomLetters/ --run';
+      exec(cmd, processing);
+      console.log('Generating final.png file');
 
   
-  // Callback for command line process
-  function processing(error, stdout, stderr) { 
-  // Error checking 
+      // Callback for command line process
+      function processing(error, stdout, stderr) { 
+      // Error checking 
       console.log(stdout);
 
-      const photoFileNameArray = ['final.png', 'shape1.png', 'shape2.png', 'shape3.png'];
-      var status = '...'; 
+        const photoFileNameArray = ['final.png', 'shape1.png', 'shape2.png', 'shape3.png'];
+        var status = '...'; 
   
-      const uploadedMedia = photoFileNameArray.map((filename) => {
-      const fileContent = fs.readFileSync(filename, { encoding: 'base64' });
-      return T.post('media/upload', { media_data: fileContent });
-      
-       });
-       console.log('Uploading photos');
+        const uploadedMedia = photoFileNameArray.map((filename) => {
+          const fileContent = fs.readFileSync(filename, { encoding: 'base64' });
+          return T.post('media/upload', { media_data: fileContent });
+        });
 
-  Promise.all(uploadedMedia).then((results) => {
-    const mediaIds = results.map(({ data }) => {
-      const { media_id_string: mediaId } = data;
+        console.log('Uploading photos');
 
-      T.post('media/metadata/create', { media_id: mediaId });
+        Promise.all(uploadedMedia).then((results) => {
+          const mediaIds = results.map(({ data }) => {
+          const { media_id_string: mediaId } = data;
 
-      return mediaId;
-    });
+          T.post('media/metadata/create', { media_id: mediaId });
+          return mediaId;
+          });
   
-      T.post('statuses/update', { status, media_ids: mediaIds });
-    });
+          T.post('statuses/update', { status, media_ids: mediaIds });
+          });
 
-    console.log('Posted');
+        console.log('Posted');
+    
+        // delete all image files 
+        fs.unlink('final.png', function (err) {
+            if (err) throw err;
+          // if no error, file has been deleted successfully
+          console.log('File deleted!');
+          }); 
+          fs.unlink('shape.png', function (err) {
+              if (err) throw err;
+             // if no error, file has been deleted successfully
+              console.log('File deleted!');
+            }); 
 
-    fs.unlink('final.png', function (err) {
-      if (err) throw err;
-      // if no error, file has been deleted successfully
-      console.log('File deleted!');
-  }); 
-  fs.unlink('shape.png', function (err) {
-    if (err) throw err;
-    // if no error, file has been deleted successfully
-    console.log('File deleted!');
-}); 
-fs.unlink('shape1.png', function (err) {
-  if (err) throw err;
-  // if no error, file has been deleted successfully
-  console.log('File deleted!');
-}); 
-fs.unlink('shape2.png', function (err) {
-  if (err) throw err;
-  // if no error, file has been deleted successfully
-  console.log('File deleted!');
-}); 
-fs.unlink('shape3.png', function (err) {
-  if (err) throw err;
-  // if no error, file has been deleted successfully
-  console.log('File deleted!');
-}); 
+          fs.unlink('shape1.png', function (err) {
+            if (err) throw err;
+            // if no error, file has been deleted successfully
+            console.log('File deleted!');
+            }); 
+
+          fs.unlink('shape2.png', function (err) {
+            if (err) throw err;
+            // if no error, file has been deleted successfully
+            console.log('File deleted!');
+            }); 
+          fs.unlink('shape3.png', function (err) {
+            if (err) throw err;
+            // if no error, file has been deleted successfully
+            console.log('File deleted!');
+            }); 
   }
 }
